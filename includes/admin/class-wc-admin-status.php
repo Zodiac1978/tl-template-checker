7c5e6ae99185fdefb5955bfd0642ed0a079d5411
Caspar Hübinger <caspar@glueckpress.com>
2015-05-04 14:00:27 +0200

    added filter for “@version” keyword

--- includes/admin/class-wc-admin-status.php
+++ includes/admin/class-wc-admin-status.php
@@ -86,13 +86,14 @@ class TPLC_Admin_Status {
 		// Make sure we catch CR-only line endings.
 		$file_data = str_replace( "\r", "\n", $file_data );
 		$version   = '';
+		$keyword   = apply_filters( 'tl_tplc_version_keyword', '@version' );
 
-		if ( preg_match( '/^[ \t\/*#@]*' . preg_quote( '@version', '/' ) . '(.*)$/mi', $file_data, $match ) && $match[1] )
+		if ( preg_match( '/^[ \t\/*#@]*' . preg_quote( $keyword, '/' ) . '(.*)$/mi', $file_data, $match ) && $match[1] )
 			$version = _cleanup_header_comment( $match[1] );
 
 		return $version ;
 	}
-	
+
 	/**
 	 * Scan the template files
 	 *
